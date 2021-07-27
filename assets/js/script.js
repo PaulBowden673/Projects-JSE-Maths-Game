e566462bfbd948f932a19a02d55496b2ba3a0280
Paul Bowden <paulbowden673@gmail.com>
2021-07-27 13:50:09 +0000

    fixed division so only whole numbers easily divisible

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,7 +92,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 / operand2 | 0, "division"];
+        return [operand1 / operand2, "division"];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -146,8 +146,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-
+    if(operand1 % operand2 === 0) {
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
+}else {
+    runGame('division');
+}
 }
